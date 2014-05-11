e7e788bf639e108dc801db72e81cdb902ea72f41
gamecreator94 <rawsonmb@gmail.com>
2014-05-11 03:49:10 -0400

    more pong changes

--- pong.cpp
+++ pong.cpp
@@ -9,13 +9,70 @@
 
 using namespace std;
 
-void winnerFunction( bool );
 int main( int argc, char* args[] )
     {
 
+    bool personPlayer = true;
+    bool computerPlayer = true;
+    bool gameOn = true;
+    SDL_Window* screen;
+    SDL_Surface* screenPrint;
+    SDL_Surface* screenOutput;
+    SDL_Surface* screenOutput2;
+    SDL_Surface* screenOutput3;
+    SDL_Event gameOver;
     ScreenBuild game;
 
     game.makeScreen();
+
+    game.getSlider();
+
+    screen = game.getScreen( screen );
+
+    screenOutput = game.getGamePieces( screenOutput );
+
+    game.getBall();
+
+    screenOutput2 = game.getGamePieces( screenOutput );
+
+    game.getenemySlider();
+
+    screenOutput3 = game.getGamePieces( screenOutput );
+
+    screenPrint = SDL_GetWindowSurface( screen );
+
+    gameOn = game.gameWorks;
+
+    // The three bool variables determines who wins, loses, or if you chose to quit.
+
+    while( personPlayer && computerPlayer && gameOn )
+        {
+
+        //while( SDL_PollEvent( &gameOver ) != 0 )
+            //{
+
+            game.followSlider();
+
+            SDL_FillRect(screenPrint, NULL, SDL_MapRGB(screenPrint -> format, 0x00, 0x00, 0x00));
+
+            SDL_BlitSurface( screenOutput, NULL, screenPrint, &game.sliderRectangle );
+
+            SDL_BlitSurface( screenOutput2, NULL, screenPrint, &game.ballRectangle );
+
+            SDL_BlitSurface( screenOutput3, NULL, screenPrint, &game.EnemySliderRectangle );
+
+            SDL_UpdateWindowSurface( screen );
+
+            if( gameOver.type == SDL_QUIT )
+                {
+
+                gameOn = false;
+
+                }// if( gameOver.type == SDL_QUIT )
+
+            //}// while( SDL_PollEvent( &gameOver ) != 0 )
+
+        }// while( personPlayer && computerPlayer )
 
     return 0;
 
--- pongImages.h
+++ pongImages.h
@@ -17,21 +17,36 @@ class ScreenBuild
     {
 
     public:
-        ScreenBuild();
-        ~ScreenBuild();
+        ScreenBuild();// constructor
+        ~ScreenBuild();// destructor
         void makeScreen();// this checks and prints the window.
-        bool getSlider( bool );
-        bool getBall( bool );
-        void setSlider();
-        void setOpponentSlider();
-        void setBall();
-        void moveSlider();
-        void moveBall();
+        void getSlider();// checks if the slider is there then inserts om the left side.
+        void getBall();// checks if the ball is there then inserts it between the sliders.
+        void getenemySlider();// checks if the enemy slider is there then inserts it on the right side.
+        SDL_Window* getScreen( SDL_Window* );// moves the screen information to the main
+        SDL_Surface* getGamePieces( SDL_Surface* );// determines which piece is placed in each spot.
+        void followSlider();// Tracks the movements of the slider and allows the player to move the slider.
+        void followEnemySlider();//Tracks the enemy slider and makes it follow the slider until the randomizer makes it miss.
+        void followBall();// follows the ball and determines where it will go also checks if it has gone out of bounds.
+        SDL_Rect sliderRectangle;
+        SDL_Rect ballRectangle;
+        SDL_Rect EnemySliderRectangle;
+        bool gameWorks;
+
+
+
 
     private:
         int screenWidth;
         int screenHeight;
-        int pngPics;
+        int sliderY;
+        int enemySliderY;
+        int ballY;
+        int ballX;
+        int bitPics;
+        SDL_Window* screen;
+        SDL_Surface* screenPrint;
+        SDL_Surface* screenOutput;
 
     };
 
@@ -44,14 +59,40 @@ ScreenBuild::ScreenBuild()
 
     screenHeight = 480;
 
+    sliderY = 200;
+
+    enemySliderY = 200;
+
+    ballY = 240;
+
+    ballX = 290;
+
+    bitPics = 0;
+
+    gameWorks = true;
+
+    SDL_Window* screen = NULL;
+
+    screenPrint = NULL;
+
+    SDL_Surface* screenOutput = NULL;
+
+    sliderRectangle.x = 16;
+
+    sliderRectangle.y = sliderY;
+
+    EnemySliderRectangle.x = 568;
+
+    EnemySliderRectangle.y = enemySliderY;
+
+    ballRectangle.x = ballX;
+
+    ballRectangle.y = ballY;
+
     }// scoreKeeper::scoreKeeper()
 
 void ScreenBuild::makeScreen()
     {
-
-    SDL_Window* screen = NULL;
-
-    SDL_Surface* screenOutput = NULL;
 
     if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
         {
@@ -59,6 +100,8 @@ void ScreenBuild::makeScreen()
         cout << "Error the SDL failed to start. Pong has exited." << endl;
 
         cout << "The type of error is:" << SDL_GetError() << endl;
+
+        gameWorks = false;
 
         }// if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
     else
@@ -73,49 +116,145 @@ void ScreenBuild::makeScreen()
 
             cout << "Pong is exiting. The error is:" << SDL_GetError() << endl;
 
+            gameWorks = false;
+
             }// if( screen == NULL )
-
-        else
-            {
-
-            pngPics = IMG_INIT_PNG;
-
-            if( !( IMG_Init( pngPics ) & pngPics) )
-                {
-
-                cout << " Error: The slider was not found. " << endl;
-
-                cout << IMG_GetError() << endl;
-
-                }// if( !( IMG_Init( pngPics ) & pngPics) )
-
-            else
-                {
-
-                screenOutput = SDL_GetWindowSurface( screen );
-
-                }// else of if( !( IMG_Init( pngPics ) & pngPics) )
-
-            /*screenOutput = SDL_GetWindowSurface( screen );
-
-            SDL_FillRect( screenOutput, NULL, SDL_MapRGB( screenOutput -> format, 0xFF, 0xFF, 0xFF ) );
-
-            SDL_UpdateWindowSurface( screen );
-
-            SDL_Delay( 2000 );*/
-
-            }// else of if( screen == NULL )
 
         }// else of if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
 
     }// void ScreenBuild::makeWindow()
 
-bool ScreenBuild::getSlider( bool )
+void ScreenBuild::getSlider()
     {
 
+    if( gameWorks )
+        {
 
+        screenOutput = SDL_LoadBMP( "pongSlider.bmp" );
 
-    }// bool ScreenBuild::getSlider( bool )
+        if( screenOutput == NULL )
+            {
+
+            cout << " An error occured stopping the image from loading. " << endl;
+
+            cout << " The error is: " << SDL_GetError();
+
+            gameWorks = false;
+
+            }// if( screenOutput == NULL )
+
+        }// if( gameWorks )
+
+    }// void ScreenBuild::getSlider()
+
+void ScreenBuild::getBall()
+    {
+
+        if( gameWorks )
+        {
+
+        screenOutput = SDL_LoadBMP( "pongBall.bmp" );
+
+        if( screenOutput == NULL )
+            {
+
+            cout << " An error occured stopping the image from loading. " << endl;
+
+            cout << " The error is: " << SDL_GetError();
+
+            gameWorks = false;
+
+            }// if( screenOutput == NULL )
+
+        }// if( gameWorks )
+
+    }// void ScreenBuild::getBall()
+
+void ScreenBuild::getenemySlider()
+    {
+
+    if( gameWorks )
+        {
+
+        screenOutput = SDL_LoadBMP( "pongSlider.bmp" );
+
+        if( screenOutput == NULL )
+            {
+
+            cout << " An error occured stopping the image from loading. " << endl;
+
+            cout << " The error is: " << SDL_GetError();
+
+            gameWorks = false;
+
+            }// if( screenOutput == NULL )
+
+        }// if( gameWorks )
+
+    }// void ScreenBuild::getenemySlider()
+
+SDL_Window* ScreenBuild::getScreen( SDL_Window* useScreen )
+    {
+
+    useScreen = screen;
+
+    return useScreen;
+
+    }// SDL_Window ScreenBuild::getScreen( SDL_Window useScreen )
+
+SDL_Surface* ScreenBuild::getGamePieces( SDL_Surface* useScreenOutput )
+    {
+
+    switch(bitPics)
+        {
+
+        case(0):
+            useScreenOutput = screenOutput;
+            bitPics = 1;
+            break;
+
+        case(1):
+            useScreenOutput = screenOutput;
+            bitPics = 0;
+            break;
+
+        default:
+            cout << "An unknown error occured when calling the game pieces." << endl;
+            break;
+
+        }// switch(bitPics)
+
+        return useScreenOutput;
+
+    }// SDL_Surface* ScreenBuild::getGamePieces( SDL_Surface* useScreenOutput )
+
+void ScreenBuild::followSlider()
+    {
+
+     SDL_Event sliderMove;
+     while(SDL_PollEvent(&sliderMove) != 0)
+     {
+         if(sliderMove.type == SDL_KEYDOWN)
+             {
+             if( sliderMove.key.keysym.sym == SDLK_UP )
+                 {
+
+                 sliderRectangle.y = sliderRectangle.y - 10;
+
+                 }// if(sliderMove.key.keysym.sym == SDLK_UP)
+
+              if( sliderMove.key.keysym.sym == SDLK_DOWN )
+                  {
+
+                  sliderRectangle.y = sliderRectangle.y + 10;
+
+                  }// if( sliderMove.key.keysym.sym == SDLK_DOWN )
+
+             }// if(sliderMove.type == SDL_KEYDOWN)
+     }
+
+    }// SDL_Rect* screenBuild::followSlider()
+
 
 ScreenBuild::~ScreenBuild()
     {
